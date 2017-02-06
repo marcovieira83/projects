5ecd82d096fc8dec9128f6ee04a979cea37473be
marcovieira83 <marco.vieira@gmail.com>
2017-02-05 23:26:07 -0200

    basic setup for angular2

---
+++ ethereum-token/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div>
+      Hello from Angular!
+    </div>
+  `
+})
+export class AppComponent {}
