3448e7af139a8180baf3fe0f4f0aba4db0d1b27b
marcovieira83 <marco.vieira@gmail.com>
2017-04-13 14:07:37 -0300

    ethereum course

---
+++ my-course/ethereum/MyToken.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.4.8;
+
+contract MyToken {
+    // XXX mapping
+    // XXX public
+    // XXX address
+    mapping (address => uint256) public balanceOf;
+
+    string name;
+
+    // XXX eventos
+    event Transfer(address from, address to, uint256 value);
+
+    function MyToken(uint256 initialSupply, string tokenName) {
+        // XXX msg.sender
+        balanceOf[msg.sender] = initialSupply;
+        name = tokenName;
+    }
+
+    function transfer(address _to, uint256 _value) {
+        // XXX throw
+        if (balanceOf[msg.sender] < _value) throw;
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+    }
+
+    // constant - read only
+    function getTokenName() constant returns (string) {
+      return name;
+    }
+}
