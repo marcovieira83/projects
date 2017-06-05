a0b708e01c0cbea5ed0ffe3fe27fd3cf3f5977a0
marcovieira83 <marco.vieira@gmail.com>
2017-06-05 15:46:41 -0300

    validating

--- flow/src/ari.js
+++ flow/src/ari.js
@@ -1,44 +1,235 @@
 var bitcoin = require('bitcoinjs-lib');
-var BigInteger = require('bigi');
-var tx = new bitcoin.TransactionBuilder(bitcoin.networks.testnet);
-tx.setVersion(2);
-
-var txId = '18ab6a1804bdbd22f878f23f832d329ba6fcf138a373897c3edc9d636b32c437';
-tx.addInput(txId, 1);
-
-var addressFrom = "mfg2bzXEJ2gwUPA5UaiN7ubnLZu1YPSgu8";
-var addressTo = "mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf";
-
-const multiplier = 100 * 1000 * 1000;
-var balance = 1.2094 * multiplier;
-var transferAmount = 0.01 * multiplier;
-var miningFee = 0.0003 * multiplier;
-var change = balance - transferAmount - miningFee;
-
-console.log("balance: " + balance);
-console.log("transferAmount: " + transferAmount);
-console.log("miningFee: " + miningFee);
-console.log("change: " + change);
-
-tx.addOutput(addressTo, transferAmount);
-tx.addOutput(addressFrom, change);
-
-var privateKeyWIF = 'cSR3WmP1y37i7xnHV3ZDjvBh6ZkPPKk2p9HQSctTb2e9t5XEEPec';
-var keyPair = bitcoin.ECPair.fromWIF(privateKeyWIF, bitcoin.networks.testnet);
-console.log("keypair.address: " + keyPair.getAddress());
-tx.sign(0, keyPair);
-
-console.log("==== signed tx ====");
-var t = tx.build();
-console.log('byteLength: ' + t.byteLength());
-console.log('hash: ' + t.getId() + '\n');
-console.log(t.toHex());
-
 var shell = require('shelljs');
-console.log("\n==== sending tx ====")
-var shellResult = shell.exec('bitcoin-cli -testnet sendrawtransaction ' + t.toHex());
-if (shellResult.code == 0) {
-  console.log('sucesso!');
-} else {
-  console.error('erro');
+var BigInteger = require('bigi');
+var BigDecimal = require("big.js");
+const MULTIPLIER = new BigDecimal(100000000);
+
+function currency(name, amountInSatoshi) {
+  return name + ": {satoshi: " + amountInSatoshi + ", btc: " + amountInSatoshi.div(MULTIPLIER) + "}";
 }
+
+function beginStep(msg, params) {
+  console.log("=================================");
+  console.log("Step: " + msg);
+  console.log("Params: " + params + "\n");
+}
+
+function endStep() {
+  console.log("\nStep completed");
+  console.log("=================================");
+}
+
+function failStep() {
+  console.log("\nStep failed");
+  console.log("=================================");
+}
+
+function sign(tx, privateKeyWIF) {
+  beginStep("3. sign", "{privateKeyWIF: " +  privateKeyWIF + "}");
+
+  var keyPair = bitcoin.ECPair.fromWIF(privateKeyWIF, bitcoin.networks.testnet);
+  tx.sign(0, keyPair);
+
+  endStep();
+}
+
+function addInput(tx, inputTxHash, inputTxVOut) {
+  beginStep("1. adding input", "{txHash: " +  inputTxHash + ", vout: " + inputTxVOut + "}");
+
+  tx.addInput(inputTxHash, inputTxVOut);
+
+  endStep();
+}
+
+function addOutputs(tx, addressFrom, balanceInBTC, recipientData) {
+  beginStep("2. adding outputs", "{addressFrom: " + addressFrom +
+    ", balanceInBTC: " + balanceInBTC + ", recipientData: " + recipientData + "}");
+
+  var balance = new BigDecimal(balanceInBTC).times(MULTIPLIER);
+  console.log(currency("balance", balance));
+
+  var totalAmount = new BigDecimal(0);
+
+  recipientData.forEach((oneRecipientData) => {
+    var amount = new BigDecimal(oneRecipientData.transferAmountInBTC).times(MULTIPLIER);
+
+    console.log("adding output  {address: " + oneRecipientData.recipientAddress
+      + ", " + currency("amount", amount) + ", " + currency("totalAmount", totalAmount) + "}");
+
+    totalAmount = totalAmount.plus(amount);
+    tx.addOutput(oneRecipientData.recipientAddress, parseInt(amount));
+  });
+
+  var miningFee = new BigDecimal(0.0003).times(MULTIPLIER);
+  var change = balance.minus(totalAmount).minus(miningFee);
+
+  console.log(currency("miningFee", miningFee));
+  console.log(currency("change", change));
+
+  tx.addOutput(addressFrom, parseInt(change));
+
+  endStep();
+}
+
+function validate(originData) {
+  beginStep("0. validating", "");
+
+  var shellResult = shell.exec('bitcoin-cli -testnet gettxout ' + originData.txHash + ' ' + originData.vOut);
+  if (shellResult.code == 0) {
+    var outJson = JSON.parse(shellResult.stdout);
+    var address = outJson.scriptPubKey.addresses[0];
+    var value = outJson.value;
+
+    if (originData.originAddress == address) {
+      console.log("Address OK");
+    } else {
+      console.log("Addresses dont match!!!");
+      failStep();
+      return false;
+    }
+
+    if (originData.currentBalanceInBTC == value) {
+      console.log("Balances OK");
+    } else {
+      console.log("Balances dont match!!!");
+      failStep();
+      return false;
+    }
+
+  } else {
+    console.error('ERROR VALIDATING TX!!!');
+    failStep();
+    return false;
+  }
+
+  endStep();
+  return true;
+}
+
+function buildTx(originData, recipientData) {
+  if (!validate(originData)) return;
+
+  var tx = new bitcoin.TransactionBuilder(bitcoin.networks.testnet);
+  tx.setVersion(2);
+
+  addInput(tx, originData.txHash, originData.vOut);
+  addOutputs(tx, originData.originAddress, originData.currentBalanceInBTC,
+    recipientData);
+  sign(tx, originData.privateKeyWIF);
+
+  beginStep("4. build tx");
+  var t = tx.build();
+
+  console.log('hash: ' + t.getId());
+  console.log('byteLength: ' + t.byteLength());
+  console.log('hex: ' + t.toHex());
+
+  endStep();
+
+  return t;
+}
+
+function sendTx(t) {
+  beginStep("5. send tx");
+
+  var shellResult = shell.exec('bitcoin-cli -testnet sendrawtransaction ' + t.toHex());
+  if (shellResult.code == 0) {
+    console.log('TX PUSHED! YEAH!!!');
+    endStep();
+  } else {
+    console.error('ERROR PUSHING TX!!!');
+    failStep();
+  }
+}
+
+var t = buildTx({
+  originAddress: 'mfg2bzXEJ2gwUPA5UaiN7ubnLZu1YPSgu8',
+  privateKeyWIF: 'cSR3WmP1y37i7xnHV3ZDjvBh6ZkPPKk2p9HQSctTb2e9t5XEEPec',
+  txHash: '58f810f7486eff1c147d5b2ccdb8c9931f3b14fb818272ee30a3534d0a814c52',
+  vOut: 20,
+  currentBalanceInBTC: 1.0249
+  },
+  [
+    {
+      recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+      transferAmountInBTC: 0.0001
+    },
+    {
+      recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+      transferAmountInBTC: 0.0002
+    },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0003
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0004
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0001
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0002
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0003
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0004
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0001
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0002
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0003
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0004
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0001
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0002
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0003
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0004
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0001
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0002
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0003
+    // },
+    // {
+    //   recipientAddress: 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf',
+    //   transferAmountInBTC: 0.0004
+    // },
+  ]
+);
+sendTx(t);
