06cb8701dbf41df3b1b1711a2bfe5ab0cda5d6b6
marcovieira83 <marco.vieira@gmail.com>
2017-06-20 10:33:06 -0300

    setting public

--- my-course/ethereum/part1/VerySimpleContract2.sol
+++ my-course/ethereum/part1/VerySimpleContract2.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.4.0;
 
 contract VerySimpleContract {
@@ -8,7 +7,7 @@ contract VerySimpleContract {
     }
 
     Statistics public statistics;
-    uint x;
+    uint public x;
 
     function VerySimpleContract() {
         setX(27);
@@ -18,12 +17,17 @@ contract VerySimpleContract {
         return x;
     }
 
+
+    function getXPlusOne() constant returns (uint _value) {
+      _value = x + 1;
+    }
+
     function setX(uint _newValue) {
         x = _newValue;
 
-        statistics.changes++;
         if (_newValue > statistics.max) {
-            statistics.max = _newValue;
+          statistics.max = _newValue;
+          statistics.changes++;
         }
     }
 }
