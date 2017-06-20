1e3971a0c72feb2f24bf58601c60a8eeb0135eab
marcovieira83 <marco.vieira@gmail.com>
2017-06-20 10:49:49 -0300

    improving token to follow ERC20

--- my-course/ethereum/part2/MyToken.sol
+++ my-course/ethereum/part2/MyToken.sol
@@ -1,33 +1,29 @@
-pragma solidity 0.4.8;
+pragma solidity ^0.4.8;
 
 contract MyToken {
-    // XXX mapping
-    // XXX public
-    // XXX address
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
     mapping (address => uint256) public balanceOf;
 
-    string public name;
-
-    // XXX eventos
     event Transfer(address from, address to, uint256 value);
 
-    function MyToken(uint256 initialSupply, string tokenName) {
-        // XXX msg.sender
+    function MyToken(uint256 initialSupply, string tokenName,
+        uint8 decimalUnits, string tokenSymbol) {
         balanceOf[msg.sender] = initialSupply;
+        totalSupply = initialSupply;
         name = tokenName;
+        symbol = tokenSymbol;
+        decimals = decimalUnits;
     }
 
     function transfer(address _to, uint256 _value) {
-        // XXX throw
         if (balanceOf[msg.sender] < _value) throw;
         if (balanceOf[_to] + _value < balanceOf[_to]) throw;
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         Transfer(msg.sender, _to, _value);
-    }
-
-    // constant - read only
-    function getTokenName() constant returns (string) {
-      return name;
     }
 }
