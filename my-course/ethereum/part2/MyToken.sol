11aec5d3158edcd140a7e7fd3102239bad8b1cc0
marcovieira83 <marco.vieira@gmail.com>
2017-11-29 17:11:15 -0200

    Atualizando para 0.4.18

--- my-course/ethereum/part2/MyToken.sol
+++ my-course/ethereum/part2/MyToken.sol
@@ -1,29 +1,28 @@
-pragma solidity ^0.4.8;
+pragma solidity 0.4.18;
 
 contract MyToken {
     string public name;
     string public symbol;
     uint8 public decimals;
-    uint256 public totalSupply;
 
     mapping (address => uint256) public balanceOf;
 
     event Transfer(address from, address to, uint256 value);
 
-    function MyToken(uint256 initialSupply, string tokenName,
-        uint8 decimalUnits, string tokenSymbol) {
-        balanceOf[msg.sender] = initialSupply;
-        totalSupply = initialSupply;
-        name = tokenName;
-        symbol = tokenSymbol;
-        decimals = decimalUnits;
+    function MyToken(uint256 _initialSupply, uint8 _decimalUnits,
+        string _tokenName, string _tokenSymbol) public {
+        balanceOf[msg.sender] = _initialSupply;
+        name = _tokenName;
+        symbol = _tokenSymbol;
+        decimals = _decimalUnits;
     }
 
-    function transfer(address _to, uint256 _value) {
-        if (balanceOf[msg.sender] < _value) throw;
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw;
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
