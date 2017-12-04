e3ddc65a493c8c20093b81cff77ca76d9be011c2
marcovieira83 <marco.vieira@gmail.com>
2017-12-04 13:58:08 -0200

    simplificando token e criando dapp do token

--- my-course/ethereum/part2/MyToken.sol
+++ my-course/ethereum/part2/MyToken.sol
@@ -1,20 +1,15 @@
 pragma solidity ^0.4.18;
 
 contract MyToken {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-
+    string public name = "MYTOKEN";
+    string public symbol = "MYT$";
+    uint8 public decimals = 2;
     mapping (address => uint256) public balanceOf;
 
-    event Transfer(address from, address to, uint256 value);
+    event Transfer(address _from, address _to, uint256 _value);
 
-    function MyToken(uint256 _initialSupply, uint8 _decimalUnits,
-        string _tokenName, string _tokenSymbol) public {
+    function MyToken(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
-        name = _tokenName;
-        symbol = _tokenSymbol;
-        decimals = _decimalUnits;
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
@@ -22,6 +17,7 @@ contract MyToken {
 
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
+
         Transfer(msg.sender, _to, _value);
         return true;
     }
